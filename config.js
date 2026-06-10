80ec0b368ebaa07f01117bcbe1962c4071b055b4
santi <1caceres.santiago5@gmail.com>
2026-06-10 00:22:03 -0300

    new file

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js  ← este archivo NO lo subís a git
+const CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbxCrKOB1w9NA-4yEf_m5PbT_MwM5oIlK-ftxPvjGbC-AkdnrV4VlbL_R7SLJ7CzqDzW/exec"
+};
